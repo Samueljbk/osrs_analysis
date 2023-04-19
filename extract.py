f232517ea6839feae7c519b876729b97ee80b4bd
Samueljbk <samjbkyle@gmail.com>
2023-04-19 20:24:39 +1200

    refactored

--- extract.py
+++ extract.py
@@ -1,20 +1,24 @@
 import json
 import os
-from loader import load_data_file
-from pprint import pprint
+
+
+def load_data_file(filename) -> dict:
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
 
 
 def load_data(directory):
-    data = {}
+    data = []
     # TODO: Make into list comprehension
     for filename in os.listdir(directory):
         if filename.endswith('.json'):
             file_path = os.path.join(directory, filename)
             file_data = load_data_file(file_path)
-            data[filename] = file_data
+            data.append(file_data)
     return data
 
 
 if __name__ == '__main__':
     data = load_data('data/')
-    pprint(data)
+    print(data)
--- loader.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-
-def load_data_file(filename) -> dict:
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    return data
