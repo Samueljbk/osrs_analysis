9803a4039bf6f905b9eff8b449e07343d9e59deb
Samueljbk <samjbkyle@gmail.com>
2023-04-21 20:35:55 +1200

    removed legacy dataclass

--- transform.py
+++ transform.py
@@ -1,38 +1,3 @@
-# "attack": {
-#                     "rank": 280980,
-#                     "level": 99,
-#                     "experience": 13040695,
-#                     "next_level_exp": 14391160,
-#                     "exp_to_next_level": 1350465
-
-
-class SkillData:
-    def __init__(
-        self,
-        skill_name: str,
-        experience: int,
-        rank: int,
-        next_level_exp: int,
-        exp_to_next_level: int,
-    ):
-        self.skill_name = skill_name
-        self.experience = experience
-        self.rank = rank
-        self.next_level_exp = next_level_exp
-        self.exp_to_next_level = exp_to_next_level
-
-    def __repr__(self):
-        return str(
-            [
-                self.skill_name,
-                self.experience,
-                self.rank,
-                self.next_level_exp,
-                self.exp_to_next_level,
-            ]
-        )
-
-
 def get_user_data(username: str, data: dict) -> dict:
     for entry in data["users"]:
         if entry["username"] == username:
@@ -40,17 +5,9 @@ def get_user_data(username: str, data: dict) -> dict:
     raise Exception("Couldn't find user")
 
 
-def get_skill_data(skill: str, user_data: dict) -> SkillData:
+def get_skill_data(skill: str, user_data: dict) -> dict:
     if skill in user_data["skills"]:
-        a = SkillData(
-            skill,
-            user_data["skills"][skill]["experience"],
-            user_data["skills"][skill]["rank"],
-            user_data["skills"][skill]["next_level_exp"],
-            user_data["skills"][skill]["exp_to_next_level"],
-        )
-        return a
-        # return user_data['skills'][skill]
+        return user_data["skills"][skill]
 
 
 def get_skill_time_series(
