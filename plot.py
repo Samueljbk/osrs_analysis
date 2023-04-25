211e4926b850799915e4f2a7d559d2e6b00c76e3
Samueljbk <samjbkyle@gmail.com>
2023-04-25 22:31:44 +1200

    Added relative and absolute functions for displaying data

--- plot.py
+++ plot.py
@@ -6,7 +6,30 @@ all_data = load_data("data/")
 user_list = ["shupwup", "telemascope", "jerome-o"]
 
 
-def main():
+def plot_relative_data():
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+
+    for user in user_list:
+        timestamp_list, skill_attribute_value_list = get_skill_time_series(
+            user, "total", all_data, "experience"
+        )
+
+        first_xp_value = skill_attribute_value_list[0]
+        offset_xp_value = [
+            xp_value - first_xp_value for xp_value in skill_attribute_value_list
+        ]
+
+        ax.plot(
+            timestamp_list, offset_xp_value, label=user
+        )  # Plot some data on the axes.
+
+    ax.set_title("Defense Level for shupwup")
+    ax.legend()
+    ax.set_ylabel("Defense Level")
+    ax.set_xlabel("Timestamp")
+
+
+def plot_absolute_data():
     fig, ax = plt.subplots()  # Create a figure containing a single axes.
 
     for user in user_list:
@@ -21,11 +44,12 @@ def main():
     ax.legend()
     ax.set_ylabel("Defense Level")
     ax.set_xlabel("Timestamp")
-    plt.show()
 
 
 if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.INFO)
-    main()
+    plot_relative_data()
+    plot_absolute_data()
+    plt.show()
