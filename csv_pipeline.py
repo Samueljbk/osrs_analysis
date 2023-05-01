eaef094f07221ddac11027cc0e4f8e645dda53dd
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-01 21:15:34 +1200

    re-wrote csv converter with pandas

--- csv_pipeline.py
+++ csv_pipeline.py
@@ -1,54 +1,32 @@
 import csv
-from extract import load_data
+from collections import defaultdict
+
 import pandas as pd
 
+from extract import load_data
 
 _DATA_DIR = "data/"
 
 
 def csv_tidy_csvwriter():
     data = load_data(_DATA_DIR)
+    data_dict = defaultdict(list)
 
-    with open("skill_data.csv", "w", newline="") as csvfile:
-        skill_writer = csv.writer(csvfile, delimiter=",", quotechar='"')
+    for scrape in data:
+        timestamp = scrape["timestamp"]
+        for user in scrape["users"]:
+            username = user["username"]
+            for skill, skill_data in user["skills"].items():
+                level = skill_data["level"]
+                experience = skill_data["experience"]
 
-        headers = ["username", "timestamp", "skill", "experience", "level"]
-        skill_writer.writerow(headers)
+                data_dict["username"].append(username)
+                data_dict["timestamp"].append(timestamp)
+                data_dict["skill"].append(skill)
+                data_dict["experience"].append(experience)
+                data_dict["level"].append(level)
 
-        for scrape in data:
-            timestamp = scrape["timestamp"]
-            for user in scrape["users"]:
-                username = user["username"]
-                for skill, skill_data in user["skills"].items():
-                    level = skill_data["level"]
-                    experience = skill_data["experience"]
-
-                    skill_writer.writerow(
-                        [
-                            username,
-                            timestamp,
-                            skill,
-                            experience,
-                            level,
-                        ]
-                    )
-
-
-# from collections import defaultdict
-
-# def csv_tidy_pandas():
-#     data = load_data(_DATA_DIR)
-#     data_dict = defaultdict(list)
-
-#     for scrape in data:
-#         timestamp = scrape["timestamp"]
-#         for user in scrape["users"]:
-#             username = user["username"]
-#             for skill, skill_data in user["skills"].items():
-#                 level = skill_data["level"]
-#                 experience = skill_data["experience"]
-
-#                 skill_writer.writerow([])
+    pd.DataFrame(data_dict).to_csv("pandas_skills.csv", index=False)
 
 
 def main():
